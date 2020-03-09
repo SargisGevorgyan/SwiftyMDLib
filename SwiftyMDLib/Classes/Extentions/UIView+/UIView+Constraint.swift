df6925692d82d5a9749e16121765e4c5e4ae2505
Sargis Gevorgian <gevorgian.sargis@gmail.com>
2020-03-09 16:37:04 +0400

    0.0.8.2

--- SwiftyMDLib/Classes/Extentions/UIView+/UIView+Constraint.swift
+++ SwiftyMDLib/Classes/Extentions/UIView+/UIView+Constraint.swift
@@ -194,21 +194,3 @@ class LayoutHelper: NSObject {
         }
     }
 }
-
-extension NSLayoutConstraint {
-    func constraintWithMultiplier(_ multiplier: CGFloat) -> NSLayoutConstraint {
-        let newConstraint = NSLayoutConstraint(
-            item: firstItem,
-            attribute: firstAttribute,
-            relatedBy: relation,
-            toItem: secondItem,
-            attribute: secondAttribute,
-            multiplier: multiplier,
-            constant: constant)
-        newConstraint.priority = priority
-        NSLayoutConstraint.deactivate([self])
-        NSLayoutConstraint.activate([newConstraint])
-        return newConstraint
-    }
-    
-}
