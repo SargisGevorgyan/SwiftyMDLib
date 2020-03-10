19afd6e2fb67b2b28f6deb57c02290bf79acd0d5
Sargis Gevorgian <gevorgian.sargis@gmail.com>
2020-03-10 16:45:07 +0400

    0.0.9.7

---
+++ SwiftyMDLib/Classes/Extentions/Managers/StoredModel+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  StoredModel+CoreDataClass.swift
+//  
+//
+//  Created by Sargis Gevorgyan on 3/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(StoredModel)
+public class StoredModel: NSManagedObject {
+
+}
---
+++ SwiftyMDLib/Classes/Extentions/Managers/StoredModel+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  StoredModel+CoreDataProperties.swift
+//  
+//
+//  Created by Sargis Gevorgyan on 3/10/20.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension StoredModel {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<StoredModel> {
+        return NSFetchRequest<StoredModel>(entityName: "StoredModel")
+    }
+
+    @NSManaged public var data: Data?
+    @NSManaged public var key: String?
+
+}
