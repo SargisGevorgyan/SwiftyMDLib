fca9b3c4c22d1e5000ae12ad7c4df05966d4df0d
Sargis Gevorgian <gevorgian.sargis@gmail.com>
2020-09-03 16:31:02 +0400

    0.0.12.5

--- SwiftyMDLib/Classes/Extentions/Date+Utilities.swift
+++ SwiftyMDLib/Classes/Extentions/Date+Utilities.swift
@@ -212,3 +212,31 @@ public extension Date {
         return isEarlierThanDate(Date())
     }
 }
+
+public extension Date {
+
+    func years(sinceDate: Date) -> Int? {
+        return Calendar.current.dateComponents([.year], from: sinceDate, to: self).year
+    }
+
+    func months(sinceDate: Date) -> Int? {
+        return Calendar.current.dateComponents([.month], from: sinceDate, to: self).month
+    }
+
+    func days(sinceDate: Date) -> Int? {
+        return Calendar.current.dateComponents([.day], from: sinceDate, to: self).day
+    }
+
+    func hours(sinceDate: Date) -> Int? {
+        return Calendar.current.dateComponents([.hour], from: sinceDate, to: self).hour
+    }
+
+    func minutes(sinceDate: Date) -> Int? {
+        return Calendar.current.dateComponents([.minute], from: sinceDate, to: self).minute
+    }
+
+    func seconds(sinceDate: Date) -> Int? {
+        return Calendar.current.dateComponents([.second], from: sinceDate, to: self).second
+    }
+
+}
