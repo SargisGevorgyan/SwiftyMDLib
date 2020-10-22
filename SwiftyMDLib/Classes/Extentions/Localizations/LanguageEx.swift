a33697d0209042308e95ceb85c6acf8238795652
Sargis Gevorgian <gevorgian.sargis@gmail.com>
2020-10-22 17:50:22 +0400

    0.0.12.10

--- SwiftyMDLib/Classes/Extentions/Localizations/LanguageEx.swift
+++ SwiftyMDLib/Classes/Extentions/Localizations/LanguageEx.swift
@@ -10,14 +10,15 @@ public enum LanguageEx: Int, Equatable,CaseIterable, Codable {
     case english = 0
     case russian = 1
     case armenian = 2
+    case chinese = 3
     
-//    func image()-> UIImage {
-//        switch self {
-//        case .armenian: return #imageLiteral(resourceName: "OperaImage")
-//        case .russian: return #imageLiteral(resourceName: "RedSquareImage")
-//        case .english: return #imageLiteral(resourceName: "BigBenImage")
-//        }
-//    }
+    //    func image()-> UIImage {
+    //        switch self {
+    //        case .armenian: return #imageLiteral(resourceName: "OperaImage")
+    //        case .russian: return #imageLiteral(resourceName: "RedSquareImage")
+    //        case .english: return #imageLiteral(resourceName: "BigBenImage")
+    //        }
+    //    }
 }
 
 
@@ -26,27 +27,30 @@ public extension LanguageEx {
     var code: String {
         switch self {
             
-        case .english:               return "en"
-        case .russian:             return "ru"
+        case .english:              return "en"
+        case .russian:              return "ru"
         case .armenian:             return "hy"
+        case .chinese:              return "zh"
         }
     }
     
     var identifier: String {
         switch self {
             
-        case .english:               return "en_US"
-        case .russian:             return "ru_RU"
+        case .english:              return "en_US"
+        case .russian:              return "ru_RU"
         case .armenian:             return "hy_AM"
+        case .chinese:              return "zh_Hant"
         }
     }
     
     var name: String {
         switch self {
             
-        case .english:               return "English"
-        case .russian:             return "Русский"
+        case .english:              return "English"
+        case .russian:              return "Русский"
         case .armenian:             return "Հայերեն"
+        case .chinese:              return "中文"
         }
     }
 }
@@ -60,6 +64,7 @@ public extension LanguageEx {
         case "en":              self = .english
         case "ru":              self = .russian
         case "hy":              self = .armenian
+        case "zh":              self = .chinese
         default:                self = .english
         }
     }
